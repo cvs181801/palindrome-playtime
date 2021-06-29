c78406b1a8235a19f526fe66e75f818d2a3bac17
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-29 16:49:10 -0700

    test out tolowercase

---
+++ index.js
@@ -0,0 +1,35 @@
+//grab the elements from the DOM
+//const string = document.getElementById("textBox");
+//const goButton = document.getElementById("go-btn");
+
+//convert all letters to lower case.  
+//Check to see if they match (use the inner 1 or 2 characters to start out both).
+//if they do, return true.  If they don't return false.
+
+function caseInsensitivePalindromeChecker(text) {
+    text.toLowerCase()
+    return text;
+//     .split("");
+//     //clone the array 
+//     const halfOriginal = text.slice(0, clonedArray.length + 1);
+//     const clonedArray = [...text];
+//     //reverse the cloned array's order
+//     clonedArray.reverse();
+//     const halfCloned = clonedArray.slice(0, clonedArray.length + 1);
+//     // check to see if the first part of the cloned array matches the first part of the original array
+//     //if it is, return true
+//         if (halfCloned === halfOriginal ) {
+//             return true;
+//             //else return false
+//         } else {
+//             return false;
+//         }     
+
+ }
+ console.log(caseInsensitivePalindromeChecker("HFLSDDSFfdd"));
+
+// //make the button work
+// goButton.addEventListener('click', (e) => {
+//     e.preventDefault;
+//     caseInsensitivePalindromeChecker(string.value);
+// })
