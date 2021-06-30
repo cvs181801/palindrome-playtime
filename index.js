c6333d8e378b95f923fe249d658e9165548a1a05
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-30 16:28:51 -0700

    add elements from the DOM

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
 //grab the elements from the DOM
-//const string = document.getElementById("textBox");
-//const goButton = document.getElementById("go-btn");
+const string = document.getElementById("textBox");
+const goButton = document.getElementById("go-btn");
 
-//convert all letters to lower case.  
-//Check to see if they match (use the inner 1 or 2 characters to start out both).
-//if they do, return true.  If they don't return false.
 let originalArray = [];
 
 function caseInsensitivePalindromeChecker(text) {
         
-       //clone the array 
+       //convert all letters to lower case.  
        originalArray = text.toLowerCase().split("");
+       //clone the array
        const clonedArray = [...originalArray];
-       //const half = originalArray.length/2 + 1;
-       //const halfOriginal = originalArray.slice(0, half);
-       //console.log("Half Original:", halfOriginal);
-       
-     //reverse the cloned array's order
+       //reverse the cloned array's order
        clonedArray.reverse();
-       console.log("Cloned Array:", clonedArray);
-       //const halfCloned = clonedArray.slice(0, half);
-       console.log("Original Array:", originalArray);
+
+            //console.log("Cloned Array:", clonedArray);
+            //console.log("Original Array:", originalArray);
        
      // check to see if the reversed cloned array matches the original array
-     
          if (JSON.stringify(clonedArray) === JSON.stringify(originalArray) ) {
     //if it is, return true
              console.log(true);
@@ -34,8 +27,8 @@ function caseInsensitivePalindromeChecker(text) {
  }
  console.log(caseInsensitivePalindromeChecker("hhOhh"));
 
-// //make the button work
-// goButton.addEventListener('click', (e) => {
-//     e.preventDefault;
-//     caseInsensitivePalindromeChecker(string.value);
-// })
+//make the button work
+goButton.addEventListener('click', (e) => {
+    e.preventDefault;
+    caseInsensitivePalindromeChecker(string.value);
+})
